72f3211507a8600ae64001481c263902d271cf2e
kahrizx <161839676+kahrizx@users.noreply.github.com>
2024-03-04 23:32:25 -0300

    Create rptarrayobj_4.js

---
+++ rptarrayobj_4.js
@@ -0,0 +1,5 @@
+let arrayElementos = ["Maçã", "Banana", "Cereja"];
+
+let primeirasLetras = arrayElementos.map(elemento => elemento.slice(0, 1));
+
+console.log("Primeiras letras dos elementos:", primeirasLetras);
