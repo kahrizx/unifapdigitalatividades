b225cb9366e2505d67b6af22e9daaa730f20e6f1
kahrizx <161839676+kahrizx@users.noreply.github.com>
2024-03-04 23:11:06 -0300

    Create variaveis_2.js

---
+++ variaveis_2.js
@@ -0,0 +1,8 @@
+let numero1 = parseFloat(prompt("Digite o primeiro número:"));
+let numero2 = parseFloat(prompt("Digite o segundo número (diferente do primeiro):"));
+let numero3 = parseFloat(prompt("Digite o terceiro número (diferente dos anteriores):"));
+let numero4 = parseFloat(prompt("Digite o quarto número (diferente dos anteriores):"));
+
+let media = (numero1 + numero2 + numero3 + numero4) / 4;
+
+console.log("A média dos números é: " + media);
