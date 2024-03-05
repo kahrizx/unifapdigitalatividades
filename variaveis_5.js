80e6801a2ba3d27dc610ca3774558e00625d98cf
kahrizx <161839676+kahrizx@users.noreply.github.com>
2024-03-04 23:14:33 -0300

    Create variaveis_5.js

---
+++ variaveis_5.js
@@ -0,0 +1,5 @@
+let valor = parseFloat(prompt("Digite o valor:"));
+let desconto = valor * 0.05;
+let valorComDesconto = valor - desconto;
+
+console.log("Valor com 5% de desconto: " + valorComDesconto);
