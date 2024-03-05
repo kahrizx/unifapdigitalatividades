11d58bb19fba5ec1e8261b9bf8e0673dcfff2c16
kahrizx <161839676+kahrizx@users.noreply.github.com>
2024-03-04 23:09:37 -0300

    Create variaveis.js

---
+++ variaveis.js
@@ -0,0 +1,6 @@
+let nome = prompt("Digite seu nome:");
+let idade = prompt("Digite sua idade:");
+let estadoCivil = confirm("Você é casado?");
+console.log("Nome: " + nome);
+console.log("Idade: " + idade);
+console.log("Estado Civil: " + (estadoCivil ? "Casado" : "Solteiro"));
