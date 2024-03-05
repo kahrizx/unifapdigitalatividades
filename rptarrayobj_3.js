807327eb461e0b6d5766f47250e621d8f9c54fbc
kahrizx <161839676+kahrizx@users.noreply.github.com>
2024-03-04 23:30:56 -0300

    Create rptarrayobj_3.js

---
+++ rptarrayobj_3.js
@@ -0,0 +1,9 @@
+let filme = {
+  nome: "Interstellar",
+  nota: 9.5,
+  genero: "Ficção Científica"
+};
+
+console.log("Nome do filme: " + filme.nome);
+console.log("Nota atribuída: " + filme.nota);
+console.log("Gênero: " + filme.genero);
