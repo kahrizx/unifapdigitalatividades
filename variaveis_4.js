c122baf090ced64a5733200f78641cab322dcacd
kahrizx <161839676+kahrizx@users.noreply.github.com>
2024-03-04 23:13:12 -0300

    Create variaveis_4.js

---
+++ variaveis_4.js
@@ -0,0 +1,6 @@
+let numero = parseFloat(prompt("Digite um número:"));
+let antecessor = numero - 1;
+let sucessor = numero + 1;
+
+console.log("Antecessor: " + antecessor);
+console.log("Sucessor: " + sucessor);
