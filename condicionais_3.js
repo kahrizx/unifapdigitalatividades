42821903d9c709f569aa1171a61d5b78bbdb9155
kahrizx <161839676+kahrizx@users.noreply.github.com>
2024-03-04 23:23:22 -0300

    Create condicionais_3.js

---
+++ condicionais_3.js
@@ -0,0 +1,10 @@
+let numero1 = parseFloat(prompt("Digite o primeiro número:"));
+let numero2 = parseFloat(prompt("Digite o segundo número:"));
+
+if (numero1 > numero2) {
+  console.log("O maior número é: " + numero1);
+} else if (numero2 > numero1) {
+  console.log("O maior número é: " + numero2);
+} else {
+  console.log("Os números são iguais.");
+}
